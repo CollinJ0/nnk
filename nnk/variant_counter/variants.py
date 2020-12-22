410786576cae6943e8d888e5a013a4b91167ee61
CollinJ0 <collinjjjj3@gmail.com>
2020-12-22 06:29:32 -0800

    commit

--- nnk/variant_counter/variants.py
+++ nnk/variant_counter/variants.py
@@ -15,11 +15,11 @@ class Variants:
         self.unique = None
         self.test='X'
         self.variants_map_to_ref = None
-    
+
     @property
     def variant_df(self):
-        return twist_to_variant_df(self.twist_file)
-    
+        return Variants.twist_to_variant_df(self.twist_file)
+
     @property
     def variant_fasta_str(self):
         df = self.variant_df
@@ -28,11 +28,11 @@ class Variants:
             return ''.join(['>Ref\n{}\n'.format(self.trimmed_ref)]+
                            ['>{}\n{}\n'.format(df.loc[i]['Position Name (Optional)'],
                                                df.loc[i]['Variant DNA']) for i in df.index])
-    
+
     @property
     def unique(self):        
         return self._unique
-            
+
     @unique.setter
     def unique(self, value):
         __full = len(list(self.variant_df['Variant DNA']))
@@ -42,7 +42,7 @@ class Variants:
         def get_test_str():
             __test_str = 'Unique' if __test else 'Not Unique'
             return __test_str
-        
+
         def get_my_name():
             ans = []
             frame = inspect.currentframe().f_back
@@ -52,13 +52,13 @@ class Variants:
                     if hash(self) == hash(var):
                         ans.append(k)
             return ans[0]
-        
+
         if __unique > __full:
             raise NameError('Input Twist Bio Excel file is incorrectly formatted: Error code 1')
-        
+
         if value == None:
             self._unique = __test
-            
+
         elif value == __test:
             self._unique = value
         else:
@@ -68,14 +68,12 @@ class Variants:
                                                                         get_my_name(),
                                                                         str(__test))))
             self._unique= __test
-            
-        
         return self._unique
-    
+
     @property
     def variants_map_to_ref(self):
         return self._variants_map_to_ref
-    
+
     @variants_map_to_ref.setter
     def variants_map_to_ref(self, value):
         if value != None and type(value)==bool:
@@ -84,34 +82,34 @@ class Variants:
             self._variants_map_to_ref = True
         else:
             self._variants_map_to_ref = False
-        
         return self._variants_map_to_ref
     
     def variant_fasta_to_file(self, fname):
         with open('./{}'.format(fname), 'w') as f:
             f.write(self.variant_fasta_str)
 
+    @staticmethod
+    def twist_to_variant_df(tf):
+        try:
+            df = pd.read_excel(tf, sheet_name='Variant Sheet').drop(0)
+            Variants.validate_df(df)
+        except:
+            raise NameError('Input Twist Bio Excel file is incorrectly formatted: Error code 2')
+        return df
 
-def twist_to_variant_df(tf):
-    try:
-        df = pd.read_excel(tf, sheet_name='Variant Sheet').drop(0)
-        validate_df(df)
-    except:
-        raise NameError('Input Twist Bio Excel file is incorrectly formatted: Error code 2')
-    return df
+    @staticmethod
+    def validate_df(_df):
+        correct_columns = ['Group Position Name',
+        'Position Name (Optional)',
+        'DNA start index',
+        'DNA end index',
+        'Upstream sequence (at least 10bp)',
+        'Downstream sequence (at least 10bp)',
+        'WT DNA',
+        'Variant DNA']
 
-def validate_df(_df):
-    correct_columns = ['Group Position Name',
-    'Position Name (Optional)',
-    'DNA start index',
-    'DNA end index',
-    'Upstream sequence (at least 10bp)',
-    'Downstream sequence (at least 10bp)',
-    'WT DNA',
-    'Variant DNA']
-    
-    assert (list(_df.columns)==correct_columns),"Input Twist Bio Excel file is incorrectly formatted"
-    return (_df)
+        assert (list(_df.columns)==correct_columns),"Input Twist Bio Excel file is incorrectly formatted"
+        return (_df)
 
 # TODO
 # if variants map to ref, need to make new variants fasta
